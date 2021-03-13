3180a561872db2b1772492621825f79d452387d5
장혜진 <aslk3366zz@naver.com>
2021-03-13 11:31:33 +0900

    입력한 아이디/비번 추출

--- app/src/main/java/com/neppplus/loginkotlintest_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginkotlintest_20210313/MainActivity.kt
@@ -2,10 +2,30 @@ package com.neppplus.loginkotlintest_20210313
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+       loginBtn.setOnClickListener {
+
+           val inputId = IDEdt.text.toString()
+           val inputPw = pwEdt.text.toString()
+
+           if(inputId == "admin" && inputPw == "qwer")
+           {
+
+
+
+
+
+       }
+
+
+
     }
 }
